5c258c38b47adf7d62b335ffab25a07ec1e03482
Alper Ak <alperyasinak1@gmail.com>
2025-10-02 11:43:49 +0300

    JsonParser: Add empty string check before parsing
    Signed-off-by: Alper Ak <alperyasinak1@gmail.com>

--- src/JsonParser.cpp
+++ src/JsonParser.cpp
@@ -5,29 +5,31 @@ DetectionResult JsonParser::parse(const std::string& jsonString)
 {
     DetectionResult detectionResult;
 
-    auto json = nlohmann::json::parse(jsonString);
+    if (!jsonString.empty()) {
+        auto json = nlohmann::json::parse(jsonString);
 
-    if (json.contains("detections") && json["detections"].is_array()) {
+        if (json.contains("detections") && json["detections"].is_array()) {
 
-        for (const auto& detectionJson : json["detections"]) {
+            for (const auto& detectionJson : json["detections"]) {
 
-            if (detectionJson.contains("classId") && detectionJson.contains("className") &&
-                detectionJson.contains("confidence") && detectionJson.contains("boundingBox") &&
-                detectionJson["boundingBox"].is_object() && detectionJson["boundingBox"].contains("x") &&
-                detectionJson["boundingBox"].contains("y") && detectionJson["boundingBox"].contains("width") &&
-                detectionJson["boundingBox"].contains("height")) {
+                if (detectionJson.contains("classId") && detectionJson.contains("className") &&
+                    detectionJson.contains("confidence") && detectionJson.contains("boundingBox") &&
+                    detectionJson["boundingBox"].is_object() && detectionJson["boundingBox"].contains("x") &&
+                    detectionJson["boundingBox"].contains("y") && detectionJson["boundingBox"].contains("width") &&
+                    detectionJson["boundingBox"].contains("height")) {
 
-                    detectionResult.detections.emplace_back(
-                    detectionJson["classId"].get<int>(),
-                    detectionJson["className"].get<std::string>(),
-                    detectionJson["confidence"].get<float>(),
-                    cv::Rect(
-                        detectionJson["boundingBox"]["x"].get<int>(),
-                        detectionJson["boundingBox"]["y"].get<int>(),
-                        detectionJson["boundingBox"]["width"].get<int>(),
-                        detectionJson["boundingBox"]["height"].get<int>()
-                    )
-                );
+                        detectionResult.detections.emplace_back(
+                        detectionJson["classId"].get<int>(),
+                        detectionJson["className"].get<std::string>(),
+                        detectionJson["confidence"].get<float>(),
+                        cv::Rect(
+                            detectionJson["boundingBox"]["x"].get<int>(),
+                            detectionJson["boundingBox"]["y"].get<int>(),
+                            detectionJson["boundingBox"]["width"].get<int>(),
+                            detectionJson["boundingBox"]["height"].get<int>()
+                        )
+                    );
+                }
             }
         }
     }

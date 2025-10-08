7ba2447db1d0e5904334ab5e45ffe08b1a8dc2bf
Alper Ak <alperyasinak1@gmail.com>
2025-10-08 17:04:20 +0300

    FrameEncoder: Add doxygen documentation
    Signed-off-by: Alper Ak <alperyasinak1@gmail.com>

--- include/FrameEncoder.hpp
+++ include/FrameEncoder.hpp
@@ -3,6 +3,13 @@
 
 #include <opencv2/imgcodecs.hpp>
 
+/**
+ * @class FrameEncoder
+ * @brief Static utility class for encoding frames to various image formats
+ *
+ * Provides stateless encoding functions for converting cv::Mat frames
+ * to compressed image formats suitable for network transmission or storage.
+ */
 class FrameEncoder {
 public:
     FrameEncoder() = delete;
@@ -11,10 +18,16 @@ public:
     FrameEncoder(FrameEncoder&&) = delete;
     FrameEncoder& operator=(FrameEncoder&&) = delete;
 
+    /**
+     * @brief Encodes a frame to JPEG format
+     * @param frame Input frame to encode
+     * @param quality JPEG quality [1-100], higher is better quality
+     * @return Encoded JPEG data as byte vector
+     */
     static std::vector<uchar> encodeJPEG(const cv::Mat& frame, int quality = defaultJPEGQuality_);
 
 private:
-    static constexpr int defaultJPEGQuality_{90};
+    static constexpr int defaultJPEGQuality_{90};   ///< Default JPEG quality setting
 };
 
 #endif

db3b4e29c083d3fac14dd36c0d7ea557c481fc54
Alper Ak <alperyasinak1@gmail.com>
2026-02-03 15:17:29 +0300

    FrameEncoder: Make header-only
    Signed-off-by: Alper Ak <alperyasinak1@gmail.com>

--- include/FrameEncoder.hpp
+++ include/FrameEncoder.hpp
@@ -1,6 +1,8 @@
 #ifndef FRAMEENCODER_HPP_
 #define FRAMEENCODER_HPP_
 
+#include <vector>
+#include <optional>
 #include <opencv2/imgcodecs.hpp>
 
 /**
@@ -24,7 +26,16 @@ public:
      * @param quality JPEG quality [1-100], higher is better quality
      * @return Encoded JPEG data as byte vector
      */
-    static std::vector<uchar> encodeJPEG(const cv::Mat& frame, int quality = defaultJPEGQuality_);
+    inline static std::optional<std::vector<uchar>> encodeJPEG(const cv::Mat& frame, int quality = defaultJPEGQuality_) {
+        if (frame.empty())
+            return std::nullopt;
+
+        std::vector<uchar> encodedFrameBuffer;
+        if (!cv::imencode(".jpg", frame, encodedFrameBuffer, {cv::IMWRITE_JPEG_QUALITY, quality}))
+            return std::nullopt;
+
+        return encodedFrameBuffer;
+    }
 
 private:
     static constexpr int defaultJPEGQuality_{90};   ///< Default JPEG quality setting

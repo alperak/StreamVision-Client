7fdac1b0320537004d89c02b568fcd510699db4c
Alper Ak <alperyasinak1@gmail.com>
2026-02-03 15:29:14 +0300

    JsonParser: Make header-only
    Signed-off-by: Alper Ak <alperyasinak1@gmail.com>

--- include/JsonParser.hpp
+++ include/JsonParser.hpp
@@ -2,7 +2,9 @@
 #define JSONPARSER_HPP_
 
 #include "DetectionData.hpp"
+
 #include <nlohmann/json.hpp>
+#include <string>
 
 /**
  * @class JsonParser
@@ -38,10 +40,39 @@ public:
      * }
      * @endcode
      */
-    static DetectionResult parse(const std::string& jsonStr);
+    inline static DetectionResult parse(const std::string& jsonString) {
+        DetectionResult detectionResult;
 
-private:
+        if (!jsonString.empty()) {
+            auto json = nlohmann::json::parse(jsonString);
 
+            if (json.contains("detections") && json["detections"].is_array()) {
+
+                for (const auto& detectionJson : json["detections"]) {
+                    // Validate all required fields before parsing
+                    if (detectionJson.contains("classId") && detectionJson.contains("className") &&
+                        detectionJson.contains("confidence") && detectionJson.contains("boundingBox") &&
+                        detectionJson["boundingBox"].is_object() && detectionJson["boundingBox"].contains("x") &&
+                        detectionJson["boundingBox"].contains("y") && detectionJson["boundingBox"].contains("width") &&
+                        detectionJson["boundingBox"].contains("height")) {
+                            // Parse and construct Detection object
+                            detectionResult.detections.emplace_back(
+                            detectionJson["classId"].get<int>(),
+                            detectionJson["className"].get<std::string>(),
+                            detectionJson["confidence"].get<float>(),
+                            cv::Rect(
+                                detectionJson["boundingBox"]["x"].get<int>(),
+                                detectionJson["boundingBox"]["y"].get<int>(),
+                                detectionJson["boundingBox"]["width"].get<int>(),
+                                detectionJson["boundingBox"]["height"].get<int>()
+                            )
+                        );
+                    }
+                }
+            }
+        }
+        return detectionResult;
+    }
 };
 
 #endif
